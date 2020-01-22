d8056c25d5e07098fd94b6e96663d04fa1869bda
parneetkaur25 <60019008+parneetkaur25@users.noreply.github.com>
2020-01-22 12:14:14 -0700

    Add files via upload

---
+++ index12.js
@@ -0,0 +1,63 @@
+function parneetsangha(p1,p2) { 
+    return p1 +" is dancing with "+p2 ;
+ }
+ console.log(parneetsangha("ram","sham"))
+ 
+ 
+ function addition(a1+a2) { 
+    return a1+a2 ;
+ }
+ console.log(addition(2,4))                     
+ 
+ 
+ function subtraction(n1,n2) { 
+    return n1-n2 ;
+ }
+ console.log(subtraction(9,5)
+ 
+ 
+ function hello(phase1+phase2) { 
+    return hello("Hello"+phase1+phase2) ;
+ }
+ console.log(hello(google,twitter)))
+ 
+ 
+ function product(x1,x2) { 
+    return x1*x2 ;
+ }
+ console.log(product(4*6))
+ 
+ 
+ function vollyball(geeta,seeta) { 
+    return geeta +" is playing with "+seeta ;
+ }
+ console.log(vollyball("geeta","seeta"))
+ 
+ 
+ function division(p1,p2) { 
+    return m1/m2 ;
+ }
+ console.log(division(6,2))
+ 
+ 
+ function studying (a,b) { 
+    return a +" is studying with "+b ;
+ }
+ console.log(stuying("rohan","mohan"))
+ 
+ 
+ 
+ function singing (a) { 
+    return p1 +" is dancing with "+p2 ;
+ }
+ return kamal" is singing "
+ console.log(singing("kamalis singing)) 
+ 
+ 
+ function numbers(n1,n2,n3) { 
+    return n1+n2+n3 ;
+ }
+ console.log(numbers(4+5+6))
+ 
+ 
+ 
